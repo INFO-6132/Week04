7703b8d5f7c1564eebb3f525daa75336ecde3461
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2022-10-06 18:53:15 -0400

    02-styling

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
 // references:
 // https://dev.to/adii9/uploading-images-to-firebase-storage-in-react-native-with-expo-workflow-24kj
+// https://www.kindacode.com/article/image-picker-in-react-native/
+// https://firebase.google.com/docs/firestore/manage-data/add-data
 import React, { useEffect, useState } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { 
-  Button, 
   Text, 
   View, 
-  StyleSheet, 
-  TextInput, 
-  ScrollView, 
-  ActivityIndicator,
-  Image
+  StyleSheet,  
+  ScrollView
  } from "react-native";
+import { 
+  Button,
+  Stack,
+  TextInput,
+  Surface
+} from "@react-native-material/core";
 import { db } from "./firebase-config";
+import { doc, setDoc } from 'firebase/firestore';
 import { onValue, push, remove, ref } from "firebase/database";
-import * as ImagePicker from "expo-image-picker"
 import Bonsai from './Bonsai';
-import { firebase } from './firebase-config';
+
 
 const dbRef = ref(db, '/bonsais');
 
@@ -30,60 +34,7 @@ const App = () => {
       thirsty: true
     });
   const bonsaisKeys = Object.keys(bonsais);
-  const [image, setImage] = useState(null);
-  const [uploading, setUploading] = useState(false);
-
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All, //specify if we want images or videos
-      allowsEditing: true,
-      aspect: [4,3],
-      quality: 1 // 0: compress - small size / 1: compress - max quality
-    });
-    console.log('result from image picker', result);
-    if(!result.cancelled) {
-      setImage(result.uri);
-    }
-  };
-
-  const uploadImage = async () => {
-    const blob = await new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.onload = function () {
-        resolve(xhr.response);
-      };
-      xhr.onerror = function () {
-        reject(new TypeError('Network request failed'));
-      };
-      xhr.responseType = 'blob';
-      xhr.open('GET', image, true);
-      xhr.send(null);
-    });
-    const ref = firebase.storage().ref().child(`Pictures/Image1`)
-    const snapshot = ref.put(blob);
-    snapshot.on(firebase.storage.TaskEvent.STATE_CHANGED,
-      ()=>{
-        setUploading(true)
-      },
-      (error) => {
-        setUploading(false);
-        console.log(error);
-        blob.close();
-        return;
-      },
-      () => {
-        snapshot.snapshot.ref.getDownloadURL().then((url)=>{
-          setUploading(false)
-          console.log("Download URL:", url)
-          setImage(url)
-          blob.close()
-          return url
-        })
-      }
-      )
-  }
-
-
+  
   useEffect(
     () => {
       return onValue(dbRef, querySnapshot => {
@@ -118,67 +69,72 @@ const App = () => {
     <ScrollView
       styles={styles.container}
       contentContainerStyle={styles.contentContainerStyle}>
-      <View>
-        {bonsaisKeys.length > 0 ? (
-          bonsaisKeys.map(key => (
-            <Bonsai
-              key={key}
-              id={key}
-              bonsai={bonsais[key]}
-            />
-          ))
-        ) : (
-          <Text>No bonsai</Text>
-        )}
-      </View>
-
-      <TextInput
-        placeholder="New Bonsai Name"
-        value={currentBonsai.name}
-        style={styles.textInput}
-        onChangeText={
-          text=>{
-            setCurrentBonsai({...currentBonsai, name:text});
-          }
-        }
-        onSubmitEditing={addNewBonsai}/>
-
-      <TextInput
-        placeholder="New Bonsai Description"
-        value={currentBonsai.description}
-        style={styles.textInput}
-        onChangeText={
-          text=>{
-            setCurrentBonsai({...currentBonsai, description:text});
-          }
-        }
-        onSubmitEditing={addNewBonsai}/>
-
-      <View>
-        <View style={{marginTop:20}}>
-          <Button
-            title="Add new bonsai"
-            onPress={addNewBonsai}
-            color="green"
-            disabled={
-              currentBonsai.name=='' ||
-              currentBonsai.description ==''
-            }/>
+      <Stack m={4} spacing={2} divider={true}>
+        <View>
+          {bonsaisKeys.length > 0 
+          ? (bonsaisKeys.map(key => (
+              <Bonsai
+                key={key}
+                id={key}
+                bonsai={bonsais[key]}
+              />
+            ))) 
+          : (<Text>No bonsai</Text>)}
         </View>
-        <View style={{marginTop:20}}>
-          <Button
-            title="Clear bonsais list"
-            onPress={clearBonsais}
-            color="red"
-            style={{marginTop:20}}/>
-        </View>
-      </View>
+      </Stack>
 
-      <View style={styles.container}>
-        {image && <Image source={{uri: image}} style={{width: 170 , height: 200}}/>}
-        <Button title='Select Image' onPress={pickImage} />
-        {!uploading ? <Button title='Upload Image' onPress={uploadImage} />: <ActivityIndicator size={'small'} color='black' />}
-      </View>
+      <Stack space>
+        <Surface 
+        elevation={4} 
+        category="medium"
+        style={{
+        justifyContent: "center",
+        alignItems: "center",
+        width: 330,
+        height: 300,
+      }}>
+          <TextInput
+            label="New Bonsai Name"
+            value={currentBonsai.name}
+            style={{width:250}}
+            onChangeText={
+              text=>{
+                setCurrentBonsai({...currentBonsai, name:text});
+              }
+            }
+            onSubmitEditing={addNewBonsai}/>
+
+          <TextInput
+            label="New Bonsai Description"
+            value={currentBonsai.description}
+            style={{width:250}}
+            onChangeText={
+              text=>{
+                setCurrentBonsai({...currentBonsai, description:text});
+              }
+            }
+            onSubmitEditing={addNewBonsai}/>
+
+          <View>
+            <View style={{marginTop:5}}>
+              <Button
+                title="Add new bonsai"
+                onPress={addNewBonsai}
+                color="green"
+                disabled={
+                  currentBonsai.name=='' ||
+                  currentBonsai.description ==''
+                }/>
+            </View>
+            <View style={{marginTop:5}}>
+              <Button
+                title="Clear bonsais list"
+                onPress={clearBonsais}
+                color="red"/>
+            </View>
+          </View>
+        </Surface>
+      </Stack>
 
     </ScrollView>
     )
@@ -210,6 +166,9 @@ const styles = StyleSheet.create({
     paddingHorizontal: 5,
     fontSize: 16
   },
+  upload: {
+    marginBottom: 5
+  }
 });
 
 export default App;
